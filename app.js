523ba7459ed3ccf5a64c3596e30b21811a71eab8
Chris Hansen <Chansen88@me.com>
2015-09-21 13:36:56 -0700

    Setup App.js for MVC

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var session = require('cookie-session');
+
+
+//var db = require('./models');
+// var loginMiddleware = require('./middleware/loginhelper');
+// var routeMiddleware = require('./middleware/routehelper');
+
+app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static("public"));
+// app.use(loginMiddleware);
+
+app.use(session({
+  maxAge: 36000000,
+  secret: 'hurhurhurhur',
+  name: 'WakeUpApp'
+}));
+
+require('./controllers/index');
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Wake UPPPPPPPP @ " + process.env.PORT || 3000);
+});
